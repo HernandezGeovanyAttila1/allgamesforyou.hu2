6c85c306b92a978c284b5bbc6ea112c07e9b9ddb
HernandezGeovanyAttila1 <zero9282g@gmail.com>
2025-11-14 11:26:48 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,7 +1,12 @@
 <?php
 // ---- SESSION SETUP (safe for cPanel) ----
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 ini_set('session.save_path', realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/tmp'));
 session_start();
+
+
 
 // ---- DATABASE CONNECTION ----
 $servername = "localhost";
