fd2612fa88432ff1c77d0e584639c45e5235a95c
HernandezGeovanyAttila1 <zero9282g@gmail.com>
2025-11-14 11:24:59 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,6 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 ini_set('session.save_path', realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/tmp'));
 session_start();
+
 
 // Redirect if not logged in
 if(!isset($_SESSION['user_id'])){
