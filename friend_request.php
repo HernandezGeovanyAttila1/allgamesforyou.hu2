cf3a04027234716502af936f6bff033e3bfd9370
HernandezGeovanyAttila1 <zero9282g@gmail.com>
2025-12-09 13:41:52 +0100

    Update friend_request.php

--- friend_request.php
+++ friend_request.php
@@ -1,1 +1,28 @@
 
+<?php
+session_start();
+if(!isset($_SESSION['user_id'])) exit();
+$servername = "localhost";
+$db_username = "skdneoaa";
+$db_password = "t3YnVb0HN**40f";
+$database = "skdneoaa_Felhasznalok";
+$conn = new mysqli($servername,$db_username,$db_password,$database);
+
+$data=json_decode(file_get_contents('php://input'),true);
+$me=$_SESSION['user_id'];
+
+if($data['action']=='request'){
+    $fid=intval($data['friend_id']);
+    $conn->query("INSERT INTO friends (user_id, friend_id, status) VALUES ($me,$fid,'pending')");
+} elseif(in_array($data['action'],['accept','decline'])){
+    $row_id=intval($data['friend_row_id']);
+    if($data['action']=='accept'){
+        $conn->query("UPDATE friends SET status='accepted' WHERE id=$row_id");
+        // Also create reciprocal friend
+        $row=$conn->query("SELECT friend_id FROM friends WHERE id=$row_id")->fetch_assoc();
+        $friend=$row['friend_id'];
+        $conn->query("INSERT INTO friends (user_id, friend_id, status) VALUES ($friend,$me,'accepted')");
+    } else {
+        $conn->query("DELETE FROM friends WHERE id=$row_id");
+    }
+}
